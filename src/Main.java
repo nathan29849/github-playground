db20b3d6875145cb5b3d4149e47222f1b2bd2b68
vans <spentmny@nate.com>
2022-02-19 17:29:19 +0900

    step3 진행완료

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("step1");
         System.out.println("step2");
-        System.out.println("step3 진행중");
+        System.out.println("step3");
     }
 }
